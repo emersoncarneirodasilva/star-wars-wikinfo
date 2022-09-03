63cbda9e67b3a1d3fe4c32a04c9257d3cf06e994
Emerson Carneiro da Silva <94311606+emersoncarneirodasilva@users.noreply.github.com>
2022-09-02 21:35:46 -0300

    Update VehiclesItem.jsx

--- src/components/vehicles/VehiclesItem.jsx
+++ src/components/vehicles/VehiclesItem.jsx
@@ -9,9 +9,9 @@ const VehiclesItem = ({ vehicle: { name, model, passengers, crew, url } }) => {
       <Card className="dark-grey shadow">
         <Card.Header className="text-warning">{name}</Card.Header>
         <Card.Body>
-          <ListGroup variant="flush"  className="mb-4 dark-theme rounded">
+          <ListGroup variant="flush" className="mb-4 dark-theme rounded">
             <ListGroup.Item><strong>Modelo: </strong>{model}</ListGroup.Item>
-            <ListGroup.Item><strong>Passengeiros: </strong>{passengers}</ListGroup.Item>
+            <ListGroup.Item><strong>Passageiros: </strong>{passengers}</ListGroup.Item>
             <ListGroup.Item><strong>Equipe Técnica: </strong>{crew}</ListGroup.Item>
           </ListGroup>
           <Button variant="outline-primary" as={Link} to={getIdFromUrl(url)}>Mais Informações</Button>
